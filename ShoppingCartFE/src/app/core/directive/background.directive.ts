4a9e9883d14f73f25399edee822a4777b91e3b0b
cbobocel <Cristian.Bobocel@endava.com>
2021-09-28 14:02:28 +0300

    directive homework

--- ShoppingCartFE/src/app/core/directive/background.directive.ts
+++ ShoppingCartFE/src/app/core/directive/background.directive.ts
@@ -1,15 +1,27 @@
-import { Directive, ElementRef, Input , OnInit} from '@angular/core';
+import { Directive, ElementRef, Input , HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
-export class BackgroundDirective implements OnInit {
+export class BackgroundDirective  {
   @Input() color: string = 'red';
 
   constructor(private el: ElementRef) { }
 
   ngOnInit() {
     this.el.nativeElement.style.backgroundColor = this.color;
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.color);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
   }
 
 }
